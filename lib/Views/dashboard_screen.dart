45288c12bb0ca6478da9a298eba712d2f3aa6e72
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-08-22 01:48:58 +0300

    Add Assets

--- lib/Views/dashboard_screen.dart
+++ lib/Views/dashboard_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_course/Widgets/adaptive_layout.dart';
+import 'package:responsive_course/Widgets/desktop_layout.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DesktopLayout(),
+      ),
+    );
   }
 }
