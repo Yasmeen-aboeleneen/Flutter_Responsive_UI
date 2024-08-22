0aeba59470c6aca6281515aa03bc7f73f48ce459
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-08-22 03:52:38 +0300

    App Styles

---
+++ lib/Utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimary = Color(0xFF064060);
+const kPrimary2 =   Color(0xFF064061);
+const kPrimaryLight = Color(0xFF4EB7F2);
+const kGrey =   Color(0xFFAAAAAA);
+const kLight =  Color(0xFFFFFFFF);
+const kVeryDark = Color.fromARGB(255, 3, 1, 2);
+const kveryWhite = Color.fromARGB(255, 255, 255, 255);
---
+++ lib/Utils/images.dart
@@ -0,0 +1,34 @@
+class Assets {
+  Assets._();
+
+  static const String imagesAvatar1 = "Assets/images/avatar_1.svg";
+
+  static const String imagesAvatar2 = "Assets/images/avatar_2.svg";
+
+  static const String imagesAvatar3 = "Assets/images/avatar_3.svg";
+
+  static const String imagesBalance = "Assets/images/balance.svg";
+
+  static const String imagesCardBackground =
+      "Assets/images/card_background.png";
+
+  static const String imagesDashboard = "Assets/images/dashboard.svg";
+
+  static const String imagesExpenses = "Assets/images/expenses.svg";
+
+  static const String imagesGallery = "Assets/images/gallery.svg";
+
+  static const String imagesIncome = "Assets/images/income.svg";
+
+  static const String imagesLogout = "Assets/images/logout.svg";
+
+  static const String imagesMyInvestments = "Assets/images/my_investments.svg";
+
+  static const String imagesMyTransctions = "Assets/images/my_transctions.svg";
+
+  static const String imagesSettings = "Assets/images/settings.svg";
+
+  static const String imagesStatistics = "Assets/images/statistics.svg";
+
+  static const String imagesWalletAccount = "Assets/images/wallet_account.svg";
+}
